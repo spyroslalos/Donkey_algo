70490928800093c769fcabe1d8f4718eca8f73c0
KizuRos <spyroslal@gmail.com>
2014-11-17 13:52:03 +0200

    GnomeSort py working

--- Python/GnomeSort.py
+++ Python/GnomeSort.py
@@ -17,3 +17,22 @@ CREATED         : Nov 16 ##:##:## CEST 2014
 
 #=============================================================================#
 """
+
+
+def GnomeSort ( slist ):
+    tmp = 0
+    i = 0
+
+    while i < len(slist):
+        if ( i == 0 or slist[i-1] <= slist[i] ):
+            i +=1
+        else:
+            #swap elements
+            slist[i-1], slist[i]  = slist[i], slist[i-1]
+            i -= 1
+
+    return slist
+
+# better replace with random generated array
+slist = [34,12,2,7,27,1,11,3]
+print GnomeSort (slist)
