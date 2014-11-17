82090a9e46d9baebf56f0d2314a54f06fc848775
KizuRos <spyroslal@gmail.com>
2014-11-17 13:18:32 +0200

    init file comm GnomeSort python

---
+++ Python/GnomeSort.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : GnomeSort.py 
+
+DESCRIPTION     : Implementation of Elementary Gnome Sorting in Python
+
+COMPLEXITY      : O(n)
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 16 ##:##:## CEST 2014
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
